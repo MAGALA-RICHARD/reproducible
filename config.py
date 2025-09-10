c4531bf75ebc6c6ba101c0cff0b6390115d03007
magala-richard <rmagala640@gmail.com>
2025-09-10 08:59:58 -0500

    first commit

--- config.py
+++ config.py
@@ -1,8 +1,5 @@
-from apsimNGpy.core.config import set_apsim_bin_path, get_apsim_bin_path, apsim_version
-from apsimNGpy.exceptions import ApsimBinPathConfigError
+from apsimNGpy.core.config import set_apsim_bin_path, get_apsim_bin_path
 from dotenv import load_dotenv
-from pathlib import Path
-import os
 
 # load env
 load_dotenv()
@@ -10,20 +7,6 @@ load_dotenv()
 CUR_BIN_PATH = get_apsim_bin_path()
 
 # get bin bath
-env_BIN_PATH = os.getenv('APSIM_BIN', '')
+env_BIN_PATH = 'APSIM2025.8.7844.0'
 
-# raise if env bin path is not valid
-
-if not os.path.exists(env_BIN_PATH):
-    raise ApsimBinPathConfigError('APSIM bin path provided in the .env file is not valid. please try again')
-
-# logic for setting bin if valid
-paths = [Path(env_BIN_PATH).resolve(), Path(CUR_BIN_PATH).resolve()]
-for index, p in enumerate(paths):
-    if not p.is_file():
-        if p.stem != 'bin':
-            p = p.joinpath('bin')
-            paths[index] = p
-
-if paths[0] != paths[1]:
-    set_apsim_bin_path(env_BIN_PATH)
+set_apsim_bin_path(env_BIN_PATH)
