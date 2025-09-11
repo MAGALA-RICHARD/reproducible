3456ce033092a6334f52cc4ddcc3ba7ba205782f
magala-richard <rmagala640@gmail.com>
2025-09-11 10:34:19 -0500

    first commite

--- config.py
+++ config.py
@@ -1,7 +1,16 @@
-from apsimNGpy.core.config import set_apsim_bin_path, get_apsim_bin_path
+from apsimNGpy.core.config import set_apsim_bin_path, get_apsim_bin_path, apsim_version
 from dotenv import load_dotenv
 import logging
 from pathlib import Path
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s | %(levelname)s | %(message)s",
+    datefmt="%H:%M:%S",
+)
+logger = logging.getLogger("app")
+
+
 # load env
 load_dotenv()
 # get a current bin path
@@ -12,12 +21,8 @@ env_BIN_PATH = Path(r'./dist/APSIM_2025.8.7844.0').resolve()
 
 set_apsim_bin_path(env_BIN_PATH)
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s | %(levelname)s | %(message)s",
-    datefmt="%H:%M:%S",
-)
-logger = logging.getLogger("app")
+version = apsim_version()
+logger.info(f"{version} successfully loaded to path")
 
 
 
