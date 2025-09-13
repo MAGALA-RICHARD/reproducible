c4420579d92ae1788e48c46cd1819bffa2d44d92
richardmagala <rmagala640@gmail.com>
2025-09-13 10:20:15 -0500

    macos versions added

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 from apsimNGpy.core.config import set_apsim_bin_path, get_apsim_bin_path, apsim_version
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 import logging
 from pathlib import Path
+import platform
 # do not move this file away from the root: reproducible
 BASE_DIR = Path(__file__).parent
 
@@ -14,14 +15,21 @@ logger = logging.getLogger("app")
 
 logger.info('setting APSIM bin path')
 # load env
-load_dotenv()
+# load_dotenv()
 # get a current bin path
 CUR_BIN_PATH = get_apsim_bin_path()
 
 base_dir = Path(__file__).parent
 # get bin bath
-env_BIN_PATH = Path(base_dir/r'bin_dist\APSIM2025.8.7844.0\bin')
-
+if platform.system() == 'Windows':
+    env_BIN_PATH = Path(base_dir/r'bin_dist\APSIM2025.8.7844.0\bin')
+elif platform.system() == 'Darwin':
+    env_BIN_PATH = Path(base_dir / base_dir/r'bin_dist/contents')
+else:
+    pt = str(input('pre compiled binaries are not provide for this operating system. please supply the path of the binaries here: '))
+    env_BIN_PATH = Path(pt)
+    if not env_BIN_PATH.exists():
+        raise NotImplementedError('path supplied does not exist ')
 set_bin = set_apsim_bin_path(env_BIN_PATH)
 
 version = apsim_version()
